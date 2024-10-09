b8cb1bb14e5c03b5a160977baf5a8fa34a719a64
wilsonwang881 <mitfans881@gmail.com>
2024-10-09 16:52:44 -0500

    recap_block:

--- prefetcher/recap-spp-camera/interface.cc
+++ prefetcher/recap-spp-camera/interface.cc
@@ -100,9 +100,9 @@ void CACHE::prefetcher_cycle_operate()
       {
         if (!champsim::operable::have_cleared_BTB
             && !champsim::operable::have_cleared_BP
-            && champsim::operable::have_cleared_prefetcher
-            && champsim::operable::cpu_side_reset_ready) {
-            //&& champsim::operable::cache_clear_counter == 7) {
+            && !champsim::operable::have_cleared_prefetcher
+            && champsim::operable::cpu_side_reset_ready
+            && champsim::operable::cache_clear_counter == 6) {
           champsim::operable::context_switch_mode = false;
           champsim::operable::cpu_side_reset_ready = false;
           champsim::operable::L2C_have_issued_context_switch_prefetches = true;
