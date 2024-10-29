cb606b960aa3a54a93e72e644c6907edeb6e601d
wilsonwang881 <mitfans881@gmail.com>
2024-10-29 15:35:05 -0500

    added eviction

--- prefetcher/stlb-pf/interface.cc
+++ prefetcher/stlb-pf/interface.cc
@@ -29,8 +29,9 @@ uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cac
 
 uint32_t CACHE::prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in)
 {
-  /*
   auto &pref = ::STLB_PF[{this, cpu}];
+
+  /*
   uint32_t blk_asid_match = metadata_in >> 2; 
   uint32_t blk_pfed = (metadata_in >> 1 & 0x1); 
   uint32_t pkt_pfed = metadata_in & 0x1;
@@ -39,8 +40,9 @@ uint32_t CACHE::prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way,
     pref.update(addr);
 
   if (blk_asid_match && !blk_pfed) 
-      pref.evict(evicted_addr);
       */
+
+  pref.evict(evicted_addr);
 
   return metadata_in;
 }
