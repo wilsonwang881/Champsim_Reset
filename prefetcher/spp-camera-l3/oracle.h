df584403e015be7a289ccf87deac0d797a04831b
wilsonwang881 <mitfans881@gmail.com>
2025-06-03 13:27:28 -0500

    LRU active

--- prefetcher/spp-camera-l3/oracle.h
+++ prefetcher/spp-camera-l3/oracle.h
@@ -25,7 +25,7 @@ namespace spp_l3 {
     constexpr static uint64_t ACCESS_LEN = 100000;
     constexpr static bool ORACLE_DEBUG_PRINT = false;
     constexpr static bool BELADY_CACHE_REPLACEMENT_POLICY_ACTIVE = false;
-    constexpr static bool REUSE_DISTANCE_REPLACEMENT_POLICY_ACTIVE = true;
+    constexpr static bool REUSE_DISTANCE_REPLACEMENT_POLICY_ACTIVE = false;
     constexpr static int MEMORY_USAGE_REDUCTION_FACTOR = 16;
     std::string L2C_PHY_ACC_FILE_NAME = "L3C_phy_acc.txt";
     std::fstream rec_file;
